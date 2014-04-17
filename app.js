7546e560147fc008ecbd86f0e71d22873fe1b61f
Dan Hems <dan.hems@fpcreative.com>
2014-04-17 17:33:58 +0100

    UNSTABLE: restructuring app code.

--- app.js
+++ app.js
@@ -45,61 +45,13 @@ require('./routes')(app);
 	}
 };
 */
-app.get('/polls', function(req, res){
-
-	Poll.find()
-		.sort({"created": -1})
-		.exec( function(err, data){
-			if(err) console.log(new Error(err));
-
-			res.send(data);
-	})
-})
-
-app.get('/polls/:pollID', function(req, res){
-
-	var pollID = req.params.pollID !== undefined ? new ObjectId(req.params.pollID) : "";
-
-	Poll.findOne({"_id": pollID})
-		.sort({"created": -1})
-		.exec( function(err, data){
-			if(err) console.log(new Error(err));
-
-			res.send(data);
-	})
-})
-
-app.post('/polls/new', function(req, res){
-	console.log(req.body);
-	Poll.create(req.body, function(err){
-		if(err) console.log(new Error(err));
-		res.send("Done");
-	})
-})
-
-app.put('/polls/vote', function(req, res){
-
-	var pollID = req.body.pollID,
-			choiceID = req.body.choiceID,
-			IP = req.ip;
-
-	var voter = {
-		"ip": IP,
-		"voted_for": choiceID
-	};
-
-	Poll.findOneAndUpdate(
-		{"_id": pollID, "choices._id": choiceID}
-	, { 
-			$inc: { 'choices.$.votes': 1 }, 
-			$push: { 'choices.$.voters': voter } 
-		}, function(err, data){
-		
-		if(err) console.log(new Error(err));
-		
-		res.json(data);
-	})
-})
 
 module.exports = app;
 
+app.get('/polls', routes.polls);
+
+app.get('/polls/:pollID', routes.poll);
+
+app.post('/polls/new', routes.create);
+
+app.put('/polls/vote', routes.vote)
--- routes/index.js
+++ routes/index.js
@@ -2,4 +2,59 @@ module.exports = function (app) {
     app.get('/', function (req, res, next) {
         res.render('index');
     });
+
+    this.polls = function(req, res){
+			Poll.find()
+				.sort({"created": -1})
+				.exec( function(err, data){
+					if(err) console.log(new Error(err));
+
+					res.send(data);
+			})
+		}
+
+		this.poll = function(req, res){
+
+			var pollID = req.params.pollID !== undefined ? new ObjectId(req.params.pollID) : "";
+
+			Poll.findOne({"_id": pollID})
+				.sort({"created": -1})
+				.exec( function(err, data){
+					if(err) console.log(new Error(err));
+
+					res.send(data);
+			})
+		}
+
+		this.create = function(req, res){
+			Poll.create(req.body, function(err){
+				if(err) console.log(new Error(err));
+				res.send("Done");
+			})
+		}
+
+		this.vote = function(req, res){
+
+			var pollID = req.body.pollID,
+					choiceID = req.body.choiceID,
+					IP = req.ip;
+
+			// Collect vote info for insertion
+			var voter = {
+				"ip": IP,
+				"voted_for": choiceID
+			}
+
+			Poll.findOneAndUpdate(
+				{"_id": pollID, "choices._id": choiceID}
+			, { 
+					$inc: { 'choices.$.votes': 1 }, 
+					$push: { 'choices.$.voters': voter } 
+				}, function(err, data){
+				
+				if(err) console.log(new Error(err));
+				
+				res.json(data);
+			})
+		}
 };
